cb4cbda60cc1a0808d812850bd6639aba97e775d
Sarah Abuzaid <sarah.muhamedemam@gmail.com>
2023-05-10 23:56:30 +0300

    init_commit

---
+++ SPI_driver_test_MCU1/Debug/mcu1.d
@@ -0,0 +1,7 @@
+mcu1.o mcu1.o: ../mcu1.c ../spi.h ../std_types.h ../gpio.h
+
+../spi.h:
+
+../std_types.h:
+
+../gpio.h:
---
+++ SPI_driver_test_MCU2/Debug/mcu2.d
@@ -0,0 +1,7 @@
+mcu2.o mcu2.o: ../mcu2.c ../spi.h ../std_types.h ../gpio.h
+
+../spi.h:
+
+../std_types.h:
+
+../gpio.h:
